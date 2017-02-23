b1e913eb03616af01aa3921fa1c8b512957f91c6
hansbrattberg <hans.brattberg@crisp.se>
2017-02-23 23:47:05 +0100

    Added check every second

--- src/start.js
+++ src/start.js
@@ -1,56 +1,47 @@
-const connectionCheck = require('./connectionCheck')
+const connectionCheck = require('./connectionCheck');
 
-var display
-var buttons
+let display;
+let buttons;
 
 try {
-  const adafruit = require('adafruit-mcp23008-ssd1306-node-driver')
+  const adafruit = require('adafruit-mcp23008-ssd1306-node-driver');
   if (adafruit.hasDriver()) {
-    console.log("Adafruit is available, so this device appears to have a display :)")
-    display = new adafruit.DisplayDriver()
-    buttons = new adafruit.ButtonDriver()
+    console.log("Adafruit is available, so this device appears to have a display :)");
+    display = new adafruit.DisplayDriver();
+    buttons = new adafruit.ButtonDriver();
   } else {
-    console.log("Adafruit is not available, so we'll fake the display using the console")
-    display = new adafruit.FakeDisplayDriver()
-    buttons = new adafruit.FakeButtonDriver()
+    console.log("Adafruit is not available, so we'll fake the display using the console");
+    display = new adafruit.FakeDisplayDriver();
+    buttons = new adafruit.FakeButtonDriver();
   }
 
 } catch (err) {
-  console.log("Failed to load Adafruit, so we'll fake the display using the console" + err)
-  display = null
-  buttons = null
+  console.log("Failed to load Adafruit, so we'll fake the display using the console" + err);
+  display = null;
+  buttons = null;
 }
 
 function showNetworkStatus() {
-
-    if (connectionCheck.checkInternetConnection()) {
-      if (display) {
-        display.text("Has connection");
-      } else {
-        console.log("Has connection");
-      }
-    } else {
-      if (display) {
-        display.text("No network connction")
-      } else {
-        console.log("No network connction");
-      }
-    }
+  if (connectionCheck.checkInternetConnection()) {
+    display.text("Has Internet Connection");
+  } else {
+    display.text("No Internet Connection!");
+  }
 }
 
 /*
-if (buttons) {
-  buttons.watchAllButtons(function(buttonId) {
-    console.log("button pressed " + buttonId)
-    if (buttonId == 0) {
-      showQrCode()
-    } else if (buttonId == 1) {
-      showRegistrationUrl()
-    } else {
-    }
-  })
-  
-}*/
+ if (buttons) {
+ buttons.watchAllButtons(function(buttonId) {
+ console.log("button pressed " + buttonId)
+ if (buttonId == 0) {
+ showQrCode()
+ } else if (buttonId == 1) {
+ showRegistrationUrl()
+ } else {
+ }
+ })
 
-showNetworkStatus();
+ }*/
+
+setInterval(showNetworkStatus, 1000);
 
